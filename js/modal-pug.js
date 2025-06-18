0c929c287e5fbe52166b6d6749aaf7be781f2dc4
Vladimir <vlad.stavros@bk.ru>
2025-06-18 19:29:37 +0300

    Исправления

---
+++ js/modal-pug.js
@@ -0,0 +1,13 @@
+const callMoadlBtn = document.querySelector('#openCallbackModal');
+const profileModaleBtn = document.querySelector('#openProfileModal');
+const profileFormOverlay = document.querySelector('#profile-modal');
+const profileFormCloseBtn = document.querySelector('.profile-modal__overlay');
+console.log(profileFormCloseBtn);
+profileModaleBtn?.addEventListener('click', (e) => {
+    e.preventDefault();
+    profileFormOverlay.display = 'block';
+});
+
+callMoadlBtn?.addEventListener('click', (e) => {
+    e.preventDefault();
+});
