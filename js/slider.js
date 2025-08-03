5cd568dc6394e1b2b5466504ba48d6210421b0eb
Vladimir <vlad.stavros@bk.ru>
2025-08-03 13:37:11 +0300

    Добавление адаптивного дизайна

--- js/slider.js
+++ js/slider.js
@@ -1,82 +1,100 @@
-// const initSlider = () => {
-//     $('.rehab-slider__container').slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         arrows: true,
-//         dots: false,
-//         // appendDots: $('.slider-nav'),
-//         prevArrow: $('.rehab-slider__arrow--prev'),
-//         nextArrow: $('.rehab-slider__arrow--next'),
-//         autoplay: true,
-//         autoplaySpeed: 5000,
-//         responsive: [
-//           {
-//             breakpoint: 768,
-//             settings: {
-//               dots: false,
-//               arrows: false
-//             }
-//           }
-//         ]
-//       });
-// }
+const initNewsSlider = () => {
+  $('.news-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 992, // lg breakpoint
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 768, // md breakpoint
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 576, // sm breakpoint
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          centerMode: true,
+          centerPadding: '20px'
+        }
+      },
+      {
+        breakpoint: 425, // xs breakpoint
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          centerMode: false, // Отключаем centerMode
+          centerPadding: '0', // Убираем отступы
+          variableWidth: false // Отключаем переменную ширину (если была включена)
+        }
+      }
+    ]
+  });
+
+  // Кастомные стрелки для новостного слайдера
+  $('.nav__arrow.prev').click(function() {
+    $('.news-slider').slick('slickPrev');
+  });
+  
+  $('.nav__arrow.next').click(function() {
+    $('.news-slider').slick('slickNext');
+  });
+}
 
 const initPensionsSlider = () => {
-    $('.pensions__slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        dots: false,
-        // appendDots: $('.pensions__slider-nav'),
-        prevArrow: $('.pensions__button--prev'),
-        nextArrow: $('.pensions__button--next'),
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 576,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows: false
-            }
-          }
-        ]
-      })
-        $('.news-slider').slick({
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 5000,
-            dots: false,
-            arrows: false,
-            infinite: true,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-        $('.nav__arrow.prev').click(function(){
-          $('.news-slider').slick('slickPrev');
-      });
-      
-      $('.nav__arrow.next').click(function(){
-          $('.news-slider').slick('slickNext');
-      });
+  $('.pensions__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow: $('.pensions__button--prev'),
+    nextArrow: $('.pensions__button--next'),
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          centerMode: true,
+          centerPadding: '20px'
+        }
+      }
+    ]
+  });
 }
 
 export const setupSlider = () => {
-    $(document).ready(function() {
-        // initSlider();
-        initPensionsSlider();
-    });
+  $(document).ready(function() {
+    if ($('.news-slider').length) {
+      initNewsSlider();
+    }
+    if ($('.pensions__slider').length) {
+      initPensionsSlider();
+    }
+  });
 };
